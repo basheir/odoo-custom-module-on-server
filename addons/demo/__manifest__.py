7b806f6e829a08973041fadbebe9682c99b6c23c
Bashir Mahad <basheirmahad@gmail.com>
2025-07-01 07:03:04 +0000

    initial commit on server

---
+++ addons/demo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Demo  Module',
+    'version': '1.0',
+    'summary': 'Demo  summary',
+    'description': 'Longer Hustle Website description',
+    'license': 'LGPL-3',
+    'author': 'Yout Name',
+    'category': 'Website',
+    'installable': True,
+    'application': False,
+}
